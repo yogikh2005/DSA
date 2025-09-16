3922570aed21e2cab9f98a507659fcf8d2acc61e
yogi <yogikh2018@gmail.com>
2025-09-16 20:50:35 +0530

    16/9/25

--- DSA/Java/pattrens printing/NumberPrintReverse.java
+++ DSA/Java/pattrens printing/NumberPrintReverse.java
@@ -4,10 +4,6 @@ import java.util.*;
     Input : 4529
 
     9   2   5   4
-    9   2   5   4
-    9   2   5   4
-    9   2   5   4
-
 */
 
 class Pattern
@@ -15,28 +11,18 @@ class Pattern
     public void Display(int iNo)
     {
         int iDigit = 0;
-        int iTemp1 = iNo;
-        int iTemp2 = iNo;
 
-        while(iTemp1 != 0)
+        while(iNo != 0)
         {
-            while(iTemp2 != 0)
-            {
-                iDigit = iTemp2 % 10;
-                System.out.print(iDigit+"\t");
-                iTemp2 = iTemp2 / 10;
-            }
-            iTemp2 = iNo;
-            
-            System.out.println();
-            iTemp1 = iTemp1 / 10;
+            iDigit = iNo % 10;
+            System.out.print(iDigit+"\t");
+            iNo = iNo / 10;
         }
-
         System.out.println();
     }
 }
-class Demo
 
+class NumberPrintReverse
 {
     public static void main(String a[])
     {
---
+++ DSA/Java/pattrens printing/StringDisplay.java
@@ -0,0 +1,22 @@
+import java.lang.*;
+
+class NumberPrint {
+    public void displayNumber(int i, int n) {
+        if (i > n) {
+            return;
+        }
+        System.out.println(i);
+        displayNumber(i + 1, n);
+    }
+}
+
+
+class RecursionDisplayNumber 
+{
+    public static void main(String args[]) 
+	{
+        NumberPrint printer = new NumberPrint(); 
+		
+        printer.displayName(1, 5); 
+    }
+}
